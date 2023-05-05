c83c582939c1e104d17fbfdd2c81eb7177834261
Eduardo Coutinho Idalgo <eduardoidalgo7@gmail.com>
2023-05-05 14:21:29 -0300

    Initial commit

--- src/Desafio.java
+++ src/Desafio.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Desafio {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int opcao = 0;
         String tipoDeConta = "Corrente";
         System.out.println("Digite o nome do titular da conta");
         String nome = scanner.nextLine();
@@ -15,5 +16,36 @@ public class Desafio {
         System.out.printf("Saldo da conta: %.2f%n", saldoDaConta);
         System.out.println("********************************************");
 
+        String menu = """
+                ** Digite sua opção **
+                1 - Consultar saldo
+                2 - Transferir valor
+                3 - Receber valor
+                4 - Sair
+                """;
+
+        while(opcao != 4) {
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            switch (opcao) {
+                case 1 -> System.out.printf("Saldo da conta: R$%.2f\n", saldoDaConta);
+                case 2 -> {
+                    System.out.println("Qual valor você deseja transferir?");
+                    double valorTransferido = scanner.nextDouble();
+                    if (valorTransferido > saldoDaConta)
+                        System.out.println("Saldo insuficiente para realizar a transferência...");
+                    else saldoDaConta -= valorTransferido;
+                    System.out.printf("Novo saldo da conta: R$%.2f\n", saldoDaConta);
+                }
+                case 3 -> {
+                    System.out.println("Valor recebido: ");
+                    double valorRecebido = scanner.nextDouble();
+                    saldoDaConta += valorRecebido;
+                    System.out.printf("Novo saldo da conta: R$%.2f\n", saldoDaConta);
+                }
+                case 4 -> System.out.println("Finalizando aplicação...");
+                default -> System.out.println("Opção inválida...");
+            }
+        }
     }
 }
